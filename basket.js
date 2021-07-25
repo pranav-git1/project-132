b16689c1d2b542f7c0117732d7e01ac6f73abcc6
pranav-git1 <77086184+pranav-git1@users.noreply.github.com>
2021-07-25 17:15:05 +0530

    uploaded project

---
+++ basket.js
@@ -0,0 +1,26 @@
+status = "";
+
+function preload() {
+    img = loadImage("basket.jfif");
+}
+
+function setup() {
+    canvas = createCanvas(400, 400);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects.....";
+}
+
+function modelLoaded() {
+    console.log("Model loaded");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+    }
+}
